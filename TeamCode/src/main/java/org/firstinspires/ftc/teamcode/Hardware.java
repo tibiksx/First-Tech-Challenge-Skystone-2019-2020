0c7f8242031e6d048d5215bd730014922ee35ef4
tiberiuureche <tiberiu.ureche@outlook.com>
2019-10-17 23:14:54 +0300

    Initial Commit - 17 October 2019 Master

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,42 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Hardware {
+
+    public DcMotor frontLeftWheel = null;
+    public DcMotor frontRightWheel = null;
+    public DcMotor backLeftWheel = null;
+    public DcMotor backRightWheel = null;
+
+    HardwareMap hwMap           =  null;
+
+    public Hardware(){
+
+    }
+
+
+    public void init(HardwareMap ahwMap) {
+
+        hwMap = ahwMap;
+
+        frontLeftWheel.setPower(0.0);
+        frontRightWheel.setPower(0.0);
+        backLeftWheel.setPower(0.0);
+        backRightWheel.setPower(0.0);
+
+        frontLeftWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontLeftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+ }
+
