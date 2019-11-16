c54974c5aaca157432e73a5974ad3459216f417d
Vlad Chira <chiravlad.andrei1234@gmail.com>
2019-11-16 11:15:35 +0200

    Refactored a few things and added some testing methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Misc/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Misc/Robot.java
@@ -3,8 +3,9 @@ package org.firstinspires.ftc.teamcode.Misc;
 import android.os.SystemClock;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.teamcode.Hardware.DriveTrain;
+import org.firstinspires.ftc.teamcode.Hardware.Hardware;
 import org.openftc.revextensions2.ExpansionHubMotor;
 import org.openftc.revextensions2.RevBulkData;
 import org.openftc.revextensions2.ExpansionHubEx;
@@ -13,43 +14,23 @@ public class Robot extends OpMode {
 
     public static boolean usingDebugger = false; //for debugging using UDP Unicast
 
-    //--------------REV STUFF-------------------------
-    private RevBulkData revExpansionMasterBulkData;
-    private RevBulkData getRevExpansionSlaveBulkData;
-    ExpansionHubEx revMaster;
-    ExpansionHubEx revSlave;
-    //------------------------------------------------
-
-
-
-    //--------------------REV MOTORS-------------------
-    ExpansionHubMotor fl = null;
-    ExpansionHubMotor fr = null;
-    ExpansionHubMotor bl = null;
-    ExpansionHubMotor br = null;
-    ExpansionHubMotor Lifter = null;
-    //-----------------------------------------------
 
     public long currTimeMillis = 0; //time in ms
 
-    private DriveTrain driveTrain;
+    public ControllerInput controllerInputA;
+    public ControllerInput controllerInputB;
 
-
+    public Hardware robot = null;
+    public HardwareMap hw = null;
     @Override
     public void init() {
         currTimeMillis = SystemClock.uptimeMillis();
 
+        controllerInputA = new ControllerInput(gamepad1);
+        controllerInputB = new ControllerInput(gamepad2);
 
-        //---------------REV MOTORS--------------------
-        revMaster = hardwareMap.get(ExpansionHubEx.class, "Expansion Hub 1");
-        revSlave = hardwareMap.get(ExpansionHubEx.class,"Expansion Hub 2");
-        fl =(ExpansionHubMotor)hardwareMap.get("FL");
-        fr = (ExpansionHubMotor)hardwareMap.get("FR");
-        bl = (ExpansionHubMotor)hardwareMap.get("BL");
-        br = (ExpansionHubMotor)hardwareMap.get("BR");
-        Lifter = (ExpansionHubMotor)hardwareMap.get("Lifter");
-
-        driveTrain = new DriveTrain(fr,fl,br,bl,Lifter);
+        robot = new Hardware();
+        robot.init(hw);
     }
 
     @Override
@@ -63,10 +44,9 @@ public class Robot extends OpMode {
     }
 
     @Override
-    public void loop() {
-        currTimeMillis = SystemClock.uptimeMillis();
-        //WIP
-    }
+    public void loop()
+    {
 
+    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/Mecanum.java
@@ -1,13 +1,37 @@
 package org.firstinspires.ftc.teamcode.TeleOps;
+
+import android.os.SystemClock;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Misc.Robot;
+
 
 @TeleOp(name = " ", group = " ")
 public class Mecanum extends Robot{
 
+    final static int LOWERED_TICKS = 0;
+    final static int FIRST_TICKS = 50;
+    final static int SECOND_TICKS = 100;
+    final static int THIRD_TICKS = 150;
+    final static int HIGH_TICKS = 200;
+
+    public enum LIFTER_STATES
+    {
+        LOWERED,
+        FIRST,
+        SECOND,
+        THIRD,
+        HIGH
+
+    }
+
+    public long currTimeMillis = 0; //time in ms
+
+     public LIFTER_STATES lifter_state;
+
     @Override
     public void init() {
         super.init();
+        lifter_state = LIFTER_STATES.LOWERED;
     }
 
     @Override
@@ -22,6 +46,57 @@ public class Mecanum extends Robot{
 
     @Override
     public void loop() {
+        currTimeMillis = SystemClock.uptimeMillis();
+
+        //Update controller custom wrapper
+        controllerInputA.update();
+        controllerInputB.update();
+
+
+        if(controllerInputA.XOnce()) //if x is pressed once
+        {
+            //move to higher state
+            LIFTER_STATES newState = nextState();
+            goToState(newState);
+
+        }
+        telemetry.addData("Lifter Ticks:  ",robot.Lifter.getCurrentPosition());
+
+
+
+    }
+
+
+
+    LIFTER_STATES nextState()
+    {
+        switch (lifter_state)
+        {
+            case FIRST: lifter_state = LIFTER_STATES.SECOND;
+            case SECOND: lifter_state = LIFTER_STATES.THIRD;
+            case THIRD: lifter_state = LIFTER_STATES.HIGH;
+            case HIGH: lifter_state = LIFTER_STATES.HIGH;
+        }
+
+        return lifter_state;
+    }
+
+    void goToSecond()
+    {
+        robot.Lifter.setTargetPosition(SECOND_TICKS);
+        robot.Lifter.setPower(0.5);
+
+    }
+    public void goToState(LIFTER_STATES newState) {
+        int position = 0;
+        if(newState == LIFTER_STATES.LOWERED) position = LOWERED_TICKS;
+        if(newState == LIFTER_STATES.FIRST) position = FIRST_TICKS;
+        if(newState == LIFTER_STATES.SECOND) position = SECOND_TICKS;
+        if(newState == LIFTER_STATES.THIRD) position = THIRD_TICKS;
+        if(newState == LIFTER_STATES.HIGH) position = HIGH_TICKS;
+
+        robot.Lifter.setTargetPosition(position);
+        robot.Lifter.setPower(0.5);
 
     }
 }
