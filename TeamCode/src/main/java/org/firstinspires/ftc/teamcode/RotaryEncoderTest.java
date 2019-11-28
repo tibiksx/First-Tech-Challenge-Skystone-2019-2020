79c009268a203918890c0dff70322f4622e9184f
tiberiuureche <tiberiu.ureche@outlook.com>
2019-11-28 15:10:57 +0200

    Rotary encoder masterclass test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotaryEncoderTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="EncoderTest", group="Pushbot")
+public class RotaryEncoderTest extends LinearOpMode {
+
+    Hardware robot = new Hardware();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        while(true) {
+            telemetry.addData("Encoder reading:", robot.test1.getCurrentPosition() / 1600);
+            telemetry.update();
+
+            // 1 full rotation - 1600 ticks.
+        }
+
+    }
+}
